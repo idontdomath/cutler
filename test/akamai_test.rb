18d4c4840a1a0ef4957907efd0f12382e3105ff6
idontdomath <ramosmejiamartin@gmail.com>
2013-12-04 14:28:22 -0300

    - added slug test for the akamai functionality

---
+++ test/akamai_test.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'test/unit'
+require 'fakeweb'
+
+require File.dirname(__FILE__) + "/../lib/cutler"
+
+class AkamaiTest < Test::Unit::TestCase
+
+	def setup
+		
+	end
+
+	def test_cloudflare_clean
+		Cutler.mode = :akamai
+		# given that the current implementation is done
+		# with a third party library, this is not covered
+		assert true
+	end
+
+end
